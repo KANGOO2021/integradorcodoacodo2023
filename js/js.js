ab9e7d1e9148327f430da1350b5b6ccf29266b60
Sergio <ser2004munoz@yahoo.com.ar>
2023-10-24 01:14:35 -0300

    IntegradorCodoaCodo-JS

--- js/js.js
+++ js/js.js
@@ -2,8 +2,11 @@
 let allContainerCart = document.querySelector('#products');
 let allContainerCartCel = document.querySelector('#products_cel');
 let containerBuyCart = document.querySelector('.card-items');
-let priceTotal = document.querySelector('.price-total')
+let priceTotal = document.querySelector('.price-total');
+let priceSubTotal = document.querySelector('#price-descuentos')
+let priceFinal = document.querySelector('#price-final')
 let amountProduct = document.querySelector('.count-product');
+
 
 
 let buyThings = [];
@@ -103,16 +106,18 @@ function loadHtml() {
                 <h5 class="cart-text">$ ${ price }</h5>
                 <div class="delete">
                     <span class="cart-cantidad">Cantidad: ${amount}</span>
-                    <i class="fa-solid fa-delete-left fa-lg delete-product" style="color: #000000;" data-id="${id}"></i>
+                    <i class="fa-solid fa-delete-left fa-lg delete-product" style="color: #000000; cursor:pointer;" data-id="${id}"></i>
                 </div>
             </div>
             
         `;
 
         containerBuyCart.appendChild(row);
+        
+        priceSubTotal.innerHTML = formatearNumero(totalCard);
 
         priceTotal.innerHTML = formatearNumero(totalCard);
-
+        
         amountProduct.innerHTML = countProduct;
         
         //console.log(totalCard)
@@ -136,18 +141,53 @@ function disabled() {
 
 function comprar() {
 
-    buyThings = [];
-    countProduct = 0;
-    amountProduct.innerHTML = 0;
-    totalCard = 0
-    disabled()
+    if (priceFinal.innerHTML == 0 || priceFinal.innerHTML == priceSubTotal.innerHTML) {
+        alert('Selecciona tu banco')
+
+    }else{
+        buyThings = [];
+        countProduct = 0;
+        amountProduct.innerHTML = 0;
+        totalCard = 0;
+        total = 0;
+        priceFinal.innerHTML = 0;
+        document.getElementById("seleccion").value = "";
+        disabled()
+
+        setTimeout(function () { alert("Gracias por su compra"); }, 500);
+       
+    }
 }
-document.getElementById("comprar").onclick = comprar;
 
 
 function formatearNumero(number) {
     return new Intl.NumberFormat("es-CL").format(number);
 }
+
+
+function precioFinal(){
+
+    let opcion = document.getElementById("seleccion").value
+    //console.log(opcion)
+
+    let total = totalCard - totalCard * opcion
+    //console.log(total)
+  
+    priceFinal.innerHTML = formatearNumero(total.toFixed(0));
+
+}
+
+function descuentos(valor) { 
+
+    document.getElementById("seleccion").value = valor;
+    precioFinal() 
+}
+
+function volver(){
+    document.getElementById("seleccion").value = "";
+    priceFinal.innerHTML = 0;
+}
+
 
 
 
